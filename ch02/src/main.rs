d7fc8ac9ae3c9923eac3986a9789be41fb20d8f0
Andrew McMiddlin <andrew@ajmccluskey.com>
2021-03-16 20:54:48 +1000

    Make guessing game loop like the book.
    Forgot about this when implementing it from memory.

--- ch02/src/main.rs
+++ ch02/src/main.rs
@@ -6,24 +6,26 @@ fn main() {
     let the_number = rand::thread_rng().gen_range(1..=100);
     println!("Guess the number!");
 
-    println!("Enter your guess:");
-    let mut guess = String::new();
+    loop {
+        println!("Enter your guess:");
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read guess");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read guess");
 
-    let guess: u32 = guess
-        .trim()
-        .parse()
-        .expect("Unable to parse guess as number");
+        let guess: u32 = guess
+            .trim()
+            .parse()
+            .expect("Unable to parse guess as number");
 
-    println!("You guessed {}. The number was {}.", guess, the_number);
-
-    let response = match guess.cmp(&the_number) {
-        Ordering::Less => "Too low",
-        Ordering::Equal => "You win!",
-        Ordering::Greater => "Too high",
-    };
-    println!("{}", response);
+        match guess.cmp(&the_number) {
+            Ordering::Less => println!("Too low"),
+            Ordering::Greater => println!("Too high"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        };
+    }
 }
