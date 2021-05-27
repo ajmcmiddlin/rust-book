84f627619750dd08fec3dfe0e87eafe91960463b
Andrew McMiddlin <andrew@ajmccluskey.com>
2021-05-27 13:11:38 +1000

    Have another crack at start of minigrep.
    Need to think harder (i.e. not think when I'm tired) about when I need
    move ownership of new things as opposed to returning references.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,13 +1,13 @@
 use std::fs;
 
 pub struct Config {
-    query: String,
-    filename: String,
+    pub query: String,
+    pub filename: String,
 }
 
-pub fn run_query(conf: &Config) -> Result<&str, &str> {
+pub fn run_query(conf: &Config) -> Result<String, String> {
     let fc = fs::read_to_string(&conf.filename)
         .expect(&format!("Unable to read file {}", conf.filename));
 
-    Err("not done")
+    Err("not done".to_string())
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,22 +4,25 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    match parse_config(&args) {
-        Ok(config) => minigrep::run_query(&config),
-        Err(e) => panic!(e)
+    let query_result = parse_config(&args).and_then(|c| minigrep::run_query(&c));
+    match query_result {
+        Ok(r) => print!("{}", r),
+        Err(e) => print!("{}", e),
     }
 }
 
-fn parse_config(args: &Vec<String>) -> Result<minigrep::Config, &str> {
+fn parse_config(args: &Vec<String>) -> Result<minigrep::Config, String> {
     let qm = args.get(1);
     let fm = args.get(2);
 
     match (qm, fm) {
-        (Some(q), Some(f)) => 
-            Ok(minigrep::Config {query: q.to_string(), filename: f.to_string()})
-        ,
-        (None, None) => Err("Expected 2 arguments: query and file"),
-        (Some(_q), None) => Err("Expected 2 arguments. Given a query but no file"),
-        (None, Some(_f)) => Err("Expected 2 arguments. Somehow given a file (second argument) but not a query (first argument)"),
+        (Some(q), Some(f)) => Ok(minigrep::Config {
+            query: q.to_string(),
+            filename: f.to_string(),
+        }),
+        _ => Err(format!(
+            "Expected 2 arguments: a query and a file. Got {} arguments.",
+            args.len()
+        )),
     }
 }
