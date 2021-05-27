90ba76a56aeb8b4ee0deddd8ebe2179a341ef515
Andrew McMiddlin <andrew@ajmccluskey.com>
2021-05-27 16:03:53 +1000

    Get fancier with error handling.
    Also move arg parsing into lib.
    
    I don't fully understand the Box stuff, but the book assures me I will
    soon. The error stuff feels like error sum types in Haskell land <3.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+use std::fmt;
 use std::fs;
 
 pub struct Config {
@@ -5,9 +7,41 @@ pub struct Config {
     pub filename: String,
 }
 
-pub fn run_query(conf: &Config) -> Result<String, String> {
+#[derive(Debug)]
+pub enum MinigrepError {
+    NotImplemented,
+    ArgCount(usize),
+}
+
+use MinigrepError::*;
+
+impl fmt::Display for MinigrepError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            NotImplemented => write!(f, "Not implemented"),
+            ArgCount(n) => write!(f, "Expected 2 arguments, a query an a file. Got {}.", n),
+        }
+    }
+}
+
+impl Error for MinigrepError {}
+
+pub fn run_query(conf: &Config) -> Result<String, Box<dyn Error>> {
     let fc = fs::read_to_string(&conf.filename)
         .expect(&format!("Unable to read file {}", conf.filename));
 
-    Err("not done".to_string())
+    Err(Box::new(NotImplemented))
+}
+
+pub fn parse_config(args: &Vec<String>) -> Result<Config, Box<dyn Error>> {
+    let qm = args.get(1);
+    let fm = args.get(2);
+
+    match (qm, fm) {
+        (Some(q), Some(f)) => Ok(Config {
+            query: q.to_string(),
+            filename: f.to_string(),
+        }),
+        _ => Err(Box::new(ArgCount(args.len() - 1))),
+    }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,25 +4,9 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query_result = parse_config(&args).and_then(|c| minigrep::run_query(&c));
+    let query_result = minigrep::parse_config(&args).and_then(|c| minigrep::run_query(&c));
     match query_result {
         Ok(r) => print!("{}", r),
         Err(e) => print!("{}", e),
-    }
-}
-
-fn parse_config(args: &Vec<String>) -> Result<minigrep::Config, String> {
-    let qm = args.get(1);
-    let fm = args.get(2);
-
-    match (qm, fm) {
-        (Some(q), Some(f)) => Ok(minigrep::Config {
-            query: q.to_string(),
-            filename: f.to_string(),
-        }),
-        _ => Err(format!(
-            "Expected 2 arguments: a query and a file. Got {} arguments.",
-            args.len()
-        )),
     }
 }
